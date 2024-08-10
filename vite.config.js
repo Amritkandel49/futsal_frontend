ef2bf133d5069e93787019c4a2488bdefd36c4d8
Amritkandel49 <amritkandel062@gmail.com>
2024-08-11 00:39:29 +0545

    sdcfv

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server :{
-  //   proxy : {
-  //     '/api' : 'http://localhost:8000'
-  //   }
-  // },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://futsal-backend-tylv.onrender.com',
+        changeOrigin: true,
+        secure: true, // since you're using https
+      }
+    }
+  },
   plugins: [react()],
 })
